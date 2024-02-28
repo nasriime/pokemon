076a90e610f0c6059c4d5c8d7e890154db3715a9
nasriime <mnasr4040@gmail.com>
2024-02-28 10:11:20 +0100

    store data in localstorage

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,28 +3,35 @@ import { useState, useEffect } from "react";
 const PokemonList = ()=>{
     const [pokemons, setPokemons] = useState([]);
 
-    const getPokemons = () => {
+    const getPokemons = (limit=20, offset=20) => {
         fetch(
           `https://pokeapi.co/api/v2/pokemon`,
         )
           .then(response => response.json())
           .then(data => {
-            console.log('data', data)
-            setPokemons(data.results)
+            console.log('data', data);
+            setPokemons(data.results);
+            localStorage.setItem("pokemons", JSON.stringify(data.results));
           })
           .catch(err => {
-            console.warn("error", err);
+            console.log("error", err);
           });
       };
 
       useEffect(()=>{
-        getPokemons()
+        var storedPokemons = JSON.parse(localStorage.getItem("pokemons"));
+
+        if(storedPokemons.length){
+          return;
+        }
+
+        getPokemons();
       }, [])
 
     return (
         <div>
             {pokemons.map(pokemon=> 
-              <div>
+              <div key={pokemon.name}>
                 {pokemon.name}
               </div>
               )}
